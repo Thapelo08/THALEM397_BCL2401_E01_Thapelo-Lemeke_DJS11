ea02bf12600b9724a6380974541b745458fe6df8
Thapelo Lemeke <thapelolemeke15@gmail.com>
2024-06-20 20:59:41 +0200

    Added navigation arrows using assets in component

--- vite-project/src/components/Navbar.jsx
+++ vite-project/src/components/Navbar.jsx
@@ -1,2 +1,16 @@
 import React from 'react'
 import { assets } from '../assets/assets'
+
+const Navbar = () => {
+    return (
+        <>
+        <div className='w-full flex justify-between item-center font-semibold'>
+        <div className='flex items-center gap-2'>
+            <img className='w-8 bg-black p-2 rounded-2xl cursor-pointer' src={assets.arrow_left} alt="" />
+            <img className='w-8 bg-black p-2 rounded-2xl cursor-pointer' src={assets.arrow_right} alt="" />
+        </div>
+
+        </div>
+        </>
+    )
+}
