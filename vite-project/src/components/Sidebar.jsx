51c7938bc77176711000fc6e214e4f3a7a991ef1
Thapelo Lemeke <thapelolemeke15@gmail.com>
2024-06-20 16:24:15 +0200

    Updated podcast section styling and content

--- vite-project/src/components/Sidebar.jsx
+++ vite-project/src/components/Sidebar.jsx
@@ -27,9 +27,13 @@ const Sidebar = () => {
                  <div className='p-4 bg-[#242424] m-2 rounded font-semibold flex-col items-start justify-start gap-1 pl-4'>
                     <h1>Create your first playlist</h1>
                     <p className='font-light' >it's easy we will help you</p>
-                      <button className>Create Playlist</button>
-
-                 </div>
+                      <button className='px-4 py-1.5 bg-white text-[15px] text-black rounded-full mt-4'>Create Playlist</button>
+                      </div>
+                      <div className='p-4 bg-[#242424] m-2 rounded font-semibold flex flex-col items-start justify-start gap-1 pl-4 mt-4'>
+                       <h1>Let's findsome podcasts to follow</h1>
+                       <p className='font-light'>we'll keep you updated on the latest episodes</p>
+                       <button className='px-4 py-1.5 bg-white text-[15px] text-black rounded-full mt-4'>Browse Podcasts</button>
+                     </div>
               </div>
         </div>
     )
