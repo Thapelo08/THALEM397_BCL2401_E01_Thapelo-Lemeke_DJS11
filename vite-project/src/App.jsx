c12f5fb1ac4ed9b825eba5edb599afb227f9e362
Thapelo Lemeke <thapelolemeke15@gmail.com>
2024-06-20 16:11:51 +0200

    Added Sidebar component with basic layout and styles

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -4,7 +4,7 @@ const App = () => {
     return (
        <div className='h-screen bg-black'>
         <div className='h-[90%] flex'>
-           <Side
+           
         </div>
 
        </div>
--- vite-project/src/components/Sidebar.jsx
+++ vite-project/src/components/Sidebar.jsx
@@ -5,7 +5,7 @@ const Sidebar = () => {
         <div className='w-[25%] h-full p-2 flex-col gap-2 text-white hidden lg:flex'>
           <div className='bg-[#121212] h-[15%] rounded flex flex-col justfy-around'>
             <div className='flex items-center gap-3 pl-8 cursor-pointer'>
-                <img src={} alt="" />
+                <img src={assets.home_icon} alt="" />
                 <p className='font-bold'>Home</p>
             </div>
             <div className='flex items-center gap-3 pl-8 cursor-pointer'>
@@ -13,7 +13,24 @@ const Sidebar = () => {
                 <p className='font-bold'>Home</p>
             </div>
             </div>
-            <div></div>
+            <div className='bg-[#121212] h-[85%] rounded'>
+             <div className='p-4 flex items-center justify-between' >
+                <div className='flex items-center gap-3'>
+                   <img src={assets.stack_icon} alt="" />
+                   <p className='font-semibold'>Your Library</p>
+                </div>
+                <div className='flex items-center gap-3'>
+                    <img className='w-5' src={assets.arrow_icon} alt=""/>
+                    <img className='w-5' src={assets.plus_icon} alt="" />
+                    </div>
+                 </div>
+                 <div className='p-4 bg-[#242424] m-2 rounded font-semibold flex-col items-start justify-start gap-1 pl-4'>
+                    <h1>Create your first playlist</h1>
+                    <p className='font-light' >it's easy we will help you</p>
+                      <button className>Create Playlist</button>
+
+                 </div>
+              </div>
         </div>
     )
 }
