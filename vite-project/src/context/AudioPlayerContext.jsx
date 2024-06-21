b5d157b8dbf7df2b68380ba707b5c42e292424bf
Thapelo Lemeke <thapelolemeke15@gmail.com>
2024-06-21 10:54:39 +0200

    "Added state variables and functions for audio player control"

--- vite-project/src/context/AudioPlayerContext.jsx
+++ vite-project/src/context/AudioPlayerContext.jsx
@@ -8,6 +8,34 @@ const seekBg = useRef();
 const seekBar = useRef();
 
 const [track,setTrack ] = useState(songsData[1]);
-const [playing,sePlaying] = uses
+const [playing,setPlaying] = useState(flase)
+const [time,seTime] = useState({
+    currentTime:{
+        second: 0,
+        minute: 0
+    },
+    totalTime: {
+        second: 0,
+        minute: 0
+    }
+});
+
+const play = () => {
+    audioRef.current.play();
+    setPlaying(true);
+}
+
+const pause = () => {
+    audioRef.current.pause();
+    setPlaying(false);
+}
+
+const playWithId = async (id) => {
+    await setTrack(songsData[id]);
+    await audioRef.current.play();
+    setPlaying(true);
+}
+
+})
 
 
