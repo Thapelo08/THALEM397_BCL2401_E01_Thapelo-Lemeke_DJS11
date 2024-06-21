ef5b828ef3bcaac82a718dee5d32209258720f09
Thapelo Lemeke <thapelolemeke15@gmail.com>
2024-06-21 10:25:15 +0200

    "Added album display component with image, title, description, and Spotify metadata"

--- vite-project/src/components/DisplayAlbum.jsx
+++ vite-project/src/components/DisplayAlbum.jsx
@@ -14,7 +14,21 @@ const displayAlbum = () => {
   return (
     <>
       <Navbar/>
-      
+      <div className='mt-10 flex gap-8 flex-col md:flex-row md:items-end'>
+         <img className='w-48 rounded' src={albumsData.img} alt="" />
+         <div className='flex flex-col'>
+            <p>Playlist</p>
+            <h2 className='text-5xl font-bold mb-4 md:text-7xl'>{albumData.name}</h2>
+            <h4>{albumsData.desc}</h4>
+            <p className='mt-1'>
+                <img className='inline-block w-5' src={assets.spotify_logo} alt="" />
+                <b> Spotify</b>
+                - 1,323,154 likes
+                - <b>50 episodes</b>
+                  about 2 hrs 30 mins
+            </p>
+        </div>
+      </div>
     </>
   )
 }
