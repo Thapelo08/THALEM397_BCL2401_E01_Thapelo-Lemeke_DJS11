e7d070a7917a886ae17e95856a87cecc95f692d5
Thapelo Lemeke <thapelolemeke15@gmail.com>
2024-06-21 10:10:50 +0200

    "Added useParams hook and albumsData import to DisplayAlbum component"

--- vite-project/src/components/DisplayAlbum.jsx
+++ vite-project/src/components/DisplayAlbum.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import Navbar from './Navbar'
+import { useParams } from 'react-router-dom'
+import { albumsData, songsData } from '../assets/assets'
+
 
 const displayAlbum = () => {
+  
+  const {id} = useParams();
+  const albumsData = albumsData[id];
+  console.log(albumsData);
+
+
   return (
     <>
       <Navbar/>
+      
     </>
   )
 }
