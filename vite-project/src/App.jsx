944b3ace0b7dcf0f0baf0355d990b99f9d60fd55
Thapelo Lemeke <thapelolemeke15@gmail.com>
2024-06-21 09:27:29 +0200

    "Added Sidebar, AudioPlayer, and Display components to App component"

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
+import Sidebar from './components/Sidebar'
+import AudioPlayer from './components/AudioPlayer'
+import Display from './components/Display'
 
 const App = () => {
-    return (
-       <div className='h-screen bg-black'>
-        <div className='h-[90%] flex'>
-           
-        </div>
-
-       </div>
-    )
-    
+  return (
+    <div className='h-screen bg-black'>
+      <div className='h-[90%] flex'>
+        <Sidebar />
+        <Display />
+      </div>
+      <AudioPlayer />
+    </div>
+  )
 }
 
-export default App;
+export default App
